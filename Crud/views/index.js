9da60265d5e875da3a1eefaf30a67fed93e1aef9
Kashyap297 <kashyap29700@gmail.com>
2024-02-18 10:57:21 +0530

    crud start

---
+++ Crud/views/index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const players = [
+    { name: "Kashyap", email: "kashyap@gmail.com", password: "465", gender: "Male", games: "Cricket", branch: "RnW-1", address: "Ahmedabad" },
+    { name: "Vikas", email: "Vikas@gmail.com", password: "798", gender: "Male", games: "Cricket, Badminton", branch: "RnW-5", address: "Surat" },
+    { name: "Om", email: "Om@gmail.com", password: "132", gender: "Male", games: "Football", branch: "RnW-6", address: "Jodhpur" },
+]
+
+// home
+app.get('/', (req, res)=>{
+    res.render('./Pages/home')
+})
+
+// List render
+app.get('/list', (req, res) => {
+    res.render('./Pages/index', { players: players })
+})
+
+// 2 - route to add player
+app.get('/addplayer', (req, res) => {
+    res.render('./Pages/form')
+})
+
+// post data to table
+app.post('/addplayer', (req, res) => {
+    const { name, email, password, gender, games, branch, address } = req.body;
+    if(name.trim() === ""){
+        console.log(req.body);
+        return res.render('./Pages/form', { error: 'Name is required', formData: req.body });
+    }
+    console.log(req.body);
+    players.push(req.body)
+    res.redirect('/list')
+})
+
+// Delete Data
+app.get('/deletePlayer/:id', (req, res)=>{
+    var id = req.params.id
+    players.splice(id, 1)
+    res.redirect('/list')
+})
+
+// edit data
+app.get('/editPlayer/:id', (req, res)=>{
+    var id = req.params.id
+    var update = players[id]
+    res.render('./Pages/editForm', {player : update})
+})
+
+// post data
+app.post('/editPlayer/:id', (req, res)=>{
+    var id = req.params.id
+    players[id] = req.body
+    res.redirect('/list')
+})
+
+app.listen(8000, () => {
+    console.log('Server Start');
+})
